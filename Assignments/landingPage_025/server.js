409ab133713ff7f16047e29e9c4aabe72d69e1a7
m-ahmadzafar <161350207+m-ahmadzafar@users.noreply.github.com>
2024-04-17 21:11:57 +0500

    Converted into Node Project & used Ejs Layouts.
    Added node, express, mongoose and express-ejs-layout for reusing code. Changed index.html to homepage.ejs and reused the navbar and CSS for other pages through layout dependency.

---
+++ Assignments/landingPage_025/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+
+//set view engine as i am rendering ejs
+app.set('view engine', 'ejs');
+
+
+
+// Set up route to serve EJS page
+// app.get('/', (req, res) => {
+//     res.render('homepage.ejs');
+// });
+
+// Use express-ejs-layouts middleware
+app.use(expressLayouts);
+
+//route which using layouts
+app.get('/', (req, res) => {
+  res.render('homepage', { title: 'Home', layout: 'layout' });
+});
+
+// app.get('/home', (req, res) => {
+//   res.render('homepage.ejs', { title: 'Home', layout: 'layout' });
+// });
+
+app.get('/contact-us', (req, res) => {
+  res.render('contact-us', { title: 'contact-us', layout: 'layout' });
+});
+
+app.get('/about-us', (req, res) => {
+  res.render('about-us', { title: 'about-us', layout: 'layout' });
+});
+
+app.get('/services', (req, res) => {
+  res.render('services', { title: 'services', layout: 'layout' });
+});
+
+
+
+// Other routes and middleware setup...
+
+
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
